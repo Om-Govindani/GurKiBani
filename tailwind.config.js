c09960f16e950f39b3e989b25694887ce66088b6
Om-Govindani <omgovindani@gmail.com>
2025-07-03 03:25:39 +0530

    files-1

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js (Tailwind v4.1+)
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        codegray: "#282C34", // 🎯 your custom color
+      },
+    },
+  },
+  plugins: [],
+}
