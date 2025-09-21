20262d556c18be1a9951ec7797bb31c4e788949d
Om-Govindani <omgovindani@gmail.com>
2025-09-21 22:57:59 +0530

    increasing maximum cache file size

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -54,7 +54,7 @@ export default defineConfig({
         ],
       },
       workbox: {
-        maximumFileSizeToCacheInBytes: 50 * 1024 * 1024,
+        maximumFileSizeToCacheInBytes: 100 * 1024 * 1024,
         globPatterns: ["**/*.{js,css,html,svg,png,json}"],
         navigateFallback: "/index.html",
         runtimeCaching: [
