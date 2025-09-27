33a3e36d98733c89228fdb861f1a6eeacd19d0e4
Om-Govindani <omgovindani@gmail.com>
2025-09-27 20:53:44 +0530

    fixing some caching issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
+
 // https://vite.dev/config/
 export default defineConfig({
   server: {
